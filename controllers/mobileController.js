301d99a23eb84edb06ce9ec171cdad71c855ab23
guhan010 <guhaneswaran6@gmail.com>
2024-05-06 16:39:14 +0530

    api complete

--- controllers/mobileController.js
+++ controllers/mobileController.js
@@ -2,16 +2,35 @@ import Samsung from "../model/Mobiles.js";
 
 export const getData = async (req, res) => {
   const da = await Samsung.find();
-  ewa.json(da);
+  res.json(da);
 };
+export const getDataById = async (req, res) => {
+  const da = await Samsung.findById(req.params.idNumber);
+  console.log(req.params);
+  res.json(da);
+};
+export const getOne = async (req, res) => {
+  const data = await Samsung.find({ brand: req.params.brand });
+  res.json(data);
+};
+// export const getOne = async (req, res) => {
+//   const data = await Samsung.findOneAndDelete({ brand: req.params.brand });
+//   res.json(data);
+// };
+
 export const postData = async (req, res) => {
   const model = new Samsung(req.body);
   const neww = await model.save();
   res.json(neww);
 };
-export const putData = (req, res) => {
-  res.send("updating data controller");
+export const putData = async (req, res) => {
+  const content = req.body;
+  const updatedData = await Samsung.findByIdAndUpdate(req.params.id, content, {
+    new: true,
+  });
+  res.json(updatedData);
 };
-export const deleteData = (req, res) => {
-  res.send("Deleting data controller");
+export const deleteData = async (req, res) => {
+  await Samsung.findByIdAndDelete(req.params.id);
+  res.send("ID Deleted");
 };
--- routes/samsung.js
+++ routes/samsung.js
@@ -2,15 +2,20 @@ import express from "express";
 import {
   deleteData,
   getData,
+  getDataById,
+  getOne,
   postData,
   putData,
 } from "../controllers/mobileController.js";
+import { get } from "mongoose";
 
 const router = express.Router();
 
 router.get("/", getData);
+router.get("/:idNumber", getDataById);
+router.get("/brand/:brand", getOne);
 router.post("/", postData);
-router.put("/", putData);
-router.delete("/", deleteData);
+router.put("/:id", putData);
+router.delete("/:id", deleteData);
 
 export default router;
