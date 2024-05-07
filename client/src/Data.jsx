733a3edc43cbc88f9b9138ca0c74bbbc5c71df9c
guhan010 <guhaneswaran6@gmail.com>
2024-05-07 13:17:28 +0530

    deleting

--- client/src/Data.jsx
+++ client/src/Data.jsx
@@ -1,14 +1,34 @@
-import React, { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Data = () => {
+  let [data, setData] = useState([]);
   useEffect(() => {
+    fetchData();
+  }, [data]);
+  function fetchData() {
     axios
       .get("https://mobile-api-eo1w.onrender.com/mobile/samsung/")
-      .then((res) => console.log(res))
+      .then((res) => setData(res.data))
       .catch((err) => console.log(err));
-  });
-  return <div>Data</div>;
+  }
+
+  async function deleteData(id) {
+    await axios.delete(
+      `https://mobile-api-eo1w.onrender.com/mobile/samsung/${id}`
+    );
+    fetchData();
+  }
+  return (
+    <div>
+      {data.map((ele) => (
+        <li key={ele._id}>
+          {ele.brand} - {ele.model} - {ele.price}{" "}
+          <button onClick={() => deleteData(ele._id)}>Delete</button>
+        </li>
+      ))}
+    </div>
+  );
 };
 
 export default Data;
